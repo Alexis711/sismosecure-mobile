caac3133bedd4e246c898e435a8a017556360e9b
Alexis.Mendez.AwsCC <amendezp@celucenter.com>
2025-02-22 12:49:15 -0600

    desarrollo del diseño del menu

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { AuthPage } from './auth.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: AuthPage
+    component: AuthPage,
+    children: [
+      {
+        path: 'inicio',
+        loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
+      },
+      {
+        path: 'ubicacion',
+        loadChildren: () => import('./ubicacion/ubicacion.module').then(m => m.UbicacionPageModule),
+      },
+      {
+        path: 'perfil',
+        loadChildren: () => import('./perfil/perfil.module').then(m => m.PerfilPageModule),
+      },
+      {
+        path: '',
+        redirectTo: '/auth/inicio',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
