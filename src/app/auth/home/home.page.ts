c6ff68a1daf4396eb11e9f3d9180ce94af4b5032
Alexis.Mendez.AwsCC <amendezp@celucenter.com>
2025-02-22 13:46:01 -0600

    Desarrolo del diseño de home, sin colores

--- src/app/auth/home/home.page.ts
+++ src/app/auth/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class HomePage implements OnInit {
-
-  constructor() { }
+  isLoadingSalones: boolean = false;
+  
+  constructor(
+    private element: ElementRef,
+    private renderer: Renderer2
+  ) { }
 
   ngOnInit() {
+  }
+
+  onCollapseOpciones(SalonID: string){
+    const desplegable = this.element.nativeElement.querySelector('#desplegarOpciones' + SalonID);
+    if (desplegable) {
+      if (desplegable.style.display === 'none' || !desplegable.style.display) {
+        this.renderer.setStyle(desplegable, 'display', 'block');
+      } else {
+        this.renderer.setStyle(desplegable, 'display', 'none');
+      }
+    }
   }
 
 }
